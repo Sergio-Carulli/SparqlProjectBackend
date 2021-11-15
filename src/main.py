e440ce3f070de88a069f1ade1ccc038184f2e702
GorkaZabarteMoreno <gorka.zabarte.moreno@alumnos.upm.es>
2021-11-15 17:51:57 +0100

    Get name, roadType, roadName, number, postalCode, neighborhood and district of a study room

--- src/main.py
+++ src/main.py
@@ -3,13 +3,9 @@ from fastapi import FastAPI
 import json
 from typing import Optional
 from SPARQLWrapper import SPARQLWrapper, JSON
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "Sparql"}
-
 
 # From all study room, get name, road_name, number, postal_code. 
 @app.get("/studies_room")
@@ -17,20 +13,25 @@ def read_study_rooms():
 
     sparql = SPARQLWrapper("http://localhost:9000/sparql")
     sparql.setQuery("""
-        PREFIX rdf: http://www.w3.org/1999/02/22-rdf-syntax-ns#
-        PREFIX rdfs: http://www.w3.org/2000/01/rdf-schema#
-        SELECT * WHERE {
-            ?sub ?pred ?obj .
-        } 
-        limit 10
-        """)
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX sr: <http://www.studyroomsmadrid.es/ns#>
+        SELECT ?name ?roadType ?roadName ?number ?postalCode ?neighborhood ?district WHERE {
+            ?x a sr:StudyRoom .
+            ?x sr:hasName ?name .
+            ?x sr:hasAddress ?y .
+            ?y sr:hasRoadType ?roadType .
+            ?y sr:hasRoadName ?roadName .
+            ?y sr:hasNumber ?number .
+            ?y sr:hasPostalCode ?postalCode .
+            ?y sr:hasNeighborhood ?neighborhood .
+            ?y sr:hasDistrict ?district
+        }
+    """)
+
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
-    results_json = json.dumps(results, indent = 4)
 
-    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    print(results_json)
-
-    return results_json
+    return results
 
 
